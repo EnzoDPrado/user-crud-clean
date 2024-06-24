87eb3bd66e90a8e8c0fe3167674b8e79857771fd
EnzoDPrado <enzo.prado@qodeless.io>
2024-06-23 21:45:54 -0300

    implementing conection with the database

---
+++ src/modules/user/repository/repository.ts
@@ -0,0 +1,36 @@
+import User from "../domain/user.entity";
+import UserGateWay from "../gateway/user.gateway";
+import { db } from "../../../knex_config/knexInstance";
+
+export default class UserRepository implements UserGateWay {
+  async add(user: User): Promise<void> {
+    try{
+        const teste = await db("tbl_user").insert({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            birthDate: user.birthDate,
+          })
+
+          console.log(teste)
+
+    }catch(error){
+        throw new Error(`Error during operation ${error}`)
+    }
+  }
+
+  async get(id: string): Promise<User> {
+    const result: User = await db('tbl_user').where({ id }).first();
+    if (!result) {
+      throw new Error("User not found.");
+    }
+    return new User({
+        id: result.id,
+        email: result.email,
+        birthDate: result.birthDate,
+        name: result.name,
+        password: result.password
+    });
+  }
+}
